2fc2945b4d16a862dad9a599dbedfc04f462f783
eun0115 <gianpaoloestacio5@gmail.com>
2022-07-21 06:14:22 +0800

    m20lte: Setup build fingerprint
    Signed-off-by: eun0115 <gianpaoloestacio5@gmail.com>

--- aosp_m20lte.mk
+++ aosp_m20lte.mk
@@ -23,3 +23,8 @@ PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ3A.220705.003.A1/8672226:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+        PRIVATE_BUILD_DESC="raven-user 12 SQ3A.220705.003 8672226 release-keys"
